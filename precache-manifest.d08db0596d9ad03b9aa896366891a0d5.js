673d514d0f02dd953b1dccae9f84ff7e05bf3006
Manolingam <manolingam1996@gmail.com>
2019-05-10 14:59:43 +0530

    Updates

---
+++ precache-manifest.d08db0596d9ad03b9aa896366891a0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c40822275289b73f7dff0da3d96ab5",
+    "url": "/border_radius_preview/index.html"
+  },
+  {
+    "revision": "3e1af77dd9921ed36c90",
+    "url": "/border_radius_preview/static/css/main.a286ef0b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/border_radius_preview/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3e1af77dd9921ed36c90",
+    "url": "/border_radius_preview/static/js/main.5beac04c.chunk.js"
+  },
+  {
+    "revision": "25cbc57b87aa1d38b930",
+    "url": "/border_radius_preview/static/js/runtime~main.83abfe1f.js"
+  }
+]);
